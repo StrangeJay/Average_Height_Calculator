91a1cd5fcad199971df59c622a665b1cd8cc6319
Jay <105195327+StrangeJay@users.noreply.github.com>
2023-01-14 12:04:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
+x = 0
+count = 0
+for y in student_heights:
+    count += 1
+    x += y
+average = x/count
+print(round(average))
+
+
